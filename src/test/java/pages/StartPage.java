6ed8bcaa9b08141f8be8989fa141e86bdd7bb20e
idzanaagi <anmorgunowa@gmail.com>
2023-03-31 00:09:18 +0300

    feat: add StartPage POM

---
+++ src/test/java/pages/StartPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class StartPage {
+    WebDriver driver;
+    public StartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[1]/button[1]")
+    private WebElement addCustomerBtn;
+    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[1]/button[2]")
+    private WebElement openAccountBtn;
+    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[1]/button[3]")
+    private WebElement customersBtn;
+    public void open(String link) {driver.get(link); }
+}
