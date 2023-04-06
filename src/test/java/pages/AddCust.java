a0a9a43dec7505fcd9f60be162aed11f6c6bde7f
idzanaagi <anmorgunowa@gmail.com>
2023-04-06 18:47:26 +0300

    refactor: replace absolute locators with relative locators

--- src/test/java/pages/AddCust.java
+++ src/test/java/pages/AddCust.java
@@ -19,21 +19,21 @@ public class AddCust {
     private final String emptyValue = "";
     private final String expectedAlertMessage = "Customer added successfully with customer id :6";
 
-    @FindBy(xpath = "/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/div[1]/input")
+    @FindBy(xpath = "//form[@name='myForm']/div[@class='form-group']/input")
     private WebElement firstNameField;
-    @FindBy(xpath = "/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/div[2]/input")
+    @FindBy(xpath = "//form[@name='myForm']/div[@class='form-group'][2]/input")
     private WebElement lastNameField;
-    @FindBy(xpath = "/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/div[3]/input")
+    @FindBy(xpath = "//form[@name='myForm']/div[@class='form-group'][3]/input")
     private WebElement postCodeField;
-    @FindBy(xpath = "/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/button")
+    @FindBy(xpath = "//form[@name='myForm']/div[@class='form-group'][3]/following-sibling::button[@type='submit']")
     private WebElement addCustomerBtn;
-    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[2]/div/div/table/tbody/tr[6]/td[1]")
+    @FindBy(xpath = "//table[contains(@class,'table')]/tbody/tr[6]/td[1]")
     private WebElement newCellWithFirstName;
-    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[2]/div/div/table/tbody/tr[6]/td[2]")
+    @FindBy(xpath = "//table[contains(@class,'table')]/tbody/tr[6]/td[2]")
     private WebElement newCellWithLastName;
-    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[2]/div/div/table/tbody/tr[6]/td[3]")
+    @FindBy(xpath = "//table[contains(@class,'table')]/tbody/tr[6]/td[3]")
     private WebElement newCellWithPostCode;
-    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[2]/div/div/table/tbody/tr[6]/td[4]")
+    @FindBy(xpath = "//table[contains(@class,'table')]/tbody/tr[6]/td[4]")
     private WebElement newCellWithAccountNumber;
 
     public void fillFirstName() {
