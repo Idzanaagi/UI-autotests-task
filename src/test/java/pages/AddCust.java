aa98c1cd8c8fdc5060075bce040860da87aa4820
idzanaagi <anmorgunowa@gmail.com>
2023-04-08 14:39:50 +0300

    feat: add allure step description (AddCust)

--- src/test/java/pages/AddCust.java
+++ src/test/java/pages/AddCust.java
@@ -1,5 +1,6 @@
 package pages;
 
+import io.qameta.allure.Allure;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -34,33 +35,43 @@ public class AddCust extends TestData {
     private WebElement newCellWithAccountNumber;
 
     public void fillFirstName(String value) {
-    firstNameField.sendKeys(value);
+        Allure.step("Fill in the first name field");
+        firstNameField.sendKeys(value);
 }
     public void fillLastName(String value) {
+        Allure.step("Fill in the first name field");
         lastNameField.sendKeys(value);
     }
     public void fillPostCode(String value) {
+        Allure.step("Fill in the post code field");
         postCodeField.sendKeys(value);
     }
     public void clickAddCustomerBtn() {
+        Allure.step("Click add customer button");
         addCustomerBtn.click();
     }
     public void confirmAction() {
+        Allure.step("Confirm the action in the modal window");
         driver.switchTo().alert().accept();
     }
     public boolean isFieldsEmpty() {
+        Allure.step("Check that the first name, last name and post code fields are empty");
         return firstNameField.getText().isEmpty() && lastNameField.getText().isEmpty() && postCodeField.getText().isEmpty();
     }
     public String getNewCeilWithFirstNameValue() {
+        Allure.step("Get the value of a new cell with the first name");
         return newCellWithFirstName.getText();
     }
     public String getNewCeilWithLastNameValue() {
+        Allure.step("Get the value of a new cell with the last name");
         return newCellWithLastName.getText();
     }
     public String getNewCeilWithPostCodeValue() {
+        Allure.step("Get the value of a new cell with the post code");
         return newCellWithPostCode.getText();
     }
     public String getNewCeilWithAccountNumberValue() {
+        Allure.step("Get the value of a new cell with the account number");
         return newCellWithAccountNumber.getText();
     }
 }
