53efec52a22d04805e500214878f932cc0fdb84d
Idzanaagi <anmorgunowa@gmail.com>
2023-04-14 13:55:00 +0300

    refactor: remove unused imports

--- src/test/java/pages/AddCust.java
+++ src/test/java/pages/AddCust.java
@@ -1,15 +1,12 @@
 package pages;
 
-import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import utils.TestData;
-
-public class AddCust extends TestData {
+public class AddCust {
 
     WebDriver driver;
 
--- src/test/java/pages/Customers.java
+++ src/test/java/pages/Customers.java
@@ -1,14 +1,11 @@
 package pages;
 
-import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import java.util.List;
 
 public class Customers {
